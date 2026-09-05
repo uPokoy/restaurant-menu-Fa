937790a29001e200f0d789073c3e220676e23140
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-06 01:32:13 +0300

    Bump cloud sync loader to V24

--- fa-cloud-sync.js
+++ fa-cloud-sync.js
@@ -1,11 +1,11 @@
-/* FA_CLOUD_SYNC_BOOTSTRAP_V23 */
+/* FA_CLOUD_SYNC_BOOTSTRAP_V24 */
 (function(){
-'use strict';
-var src='fa-cloud-sync-v18.js?build=23.1';
-var s=document.createElement('script');
-s.src=src;
-s.async=false;
-s.onload=function(){console.info('[FA] Cloud sync V23 loaded')};
-s.onerror=function(){console.error('[FA] Cloud sync V23 failed to load',src)};
-document.head.appendChild(s);
+  'use strict';
+  var src='fa-cloud-sync-v18.js?build=24.1';
+  var s=document.createElement('script');
+  s.src=src;
+  s.async=false;
+  s.onload=function(){console.info('[FA] Cloud sync V24 loaded')};
+  s.onerror=function(){console.error('[FA] Cloud sync V24 failed to load',src)};
+  document.head.appendChild(s);
 })();
