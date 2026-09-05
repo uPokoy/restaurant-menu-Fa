bca5c4e4c72381ba600262d342276c0cae58b7d3
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-06 01:41:37 +0300

    V26 bootstrap: cache bust storage sync

--- fa-cloud-sync.js
+++ fa-cloud-sync.js
@@ -1,11 +1,11 @@
-/* FA_CLOUD_SYNC_BOOTSTRAP_V25 */
+/* FA_CLOUD_SYNC_BOOTSTRAP_V26 */
 (function(){
   'use strict';
-  var src='fa-cloud-sync-v18.js?build=25.1';
+  var src='fa-cloud-sync-v18.js?build=26.1';
   var s=document.createElement('script');
   s.src=src;
   s.async=false;
-  s.onload=function(){console.info('[FA] Cloud sync V25 Realtime loaded')};
-  s.onerror=function(){console.error('[FA] Cloud sync V25 Realtime failed to load',src)};
+  s.onload=function(){console.info('[FA] Cloud sync V26 Storage loaded')};
+  s.onerror=function(){console.error('[FA] Cloud sync V26 Storage failed to load',src)};
   document.head.appendChild(s);
 })();
