ad4806e9f4035f9be7d8cd6c6f9bc2a32d9b1ddb
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-06 01:18:45 +0300

    Bump cloud sync cache version to V20

--- fa-cloud-sync.js
+++ fa-cloud-sync.js
@@ -1,11 +1,11 @@
-/* FA_CLOUD_SYNC_BOOTSTRAP_V19 */
+/* FA_CLOUD_SYNC_BOOTSTRAP_V20 */
 (function(){
   'use strict';
-  var src='fa-cloud-sync-v18.js?build=19.1';
+  var src='fa-cloud-sync-v18.js?build=20.1';
   var s=document.createElement('script');
   s.src=src;
   s.async=false;
-  s.onload=function(){console.info('[FA] Cloud sync V19 loaded')};
-  s.onerror=function(){console.error('[FA] Cloud sync V19 failed to load',src)};
+  s.onload=function(){console.info('[FA] Cloud sync V20 loaded')};
+  s.onerror=function(){console.error('[FA] Cloud sync V20 failed to load',src)};
   document.head.appendChild(s);
 })();
