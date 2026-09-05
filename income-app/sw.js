ead37aa085de8904d90686f27311d0ea371a1f50
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-05 23:23:05 +0300

    Add income app service worker

---
+++ income-app/sw.js
@@ -0,0 +1,1 @@
+const CACHE='income-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
