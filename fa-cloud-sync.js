788f2574175a87d63f4e6a738500cd0027e24009
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-06 01:24:19 +0300

    Принудительно обновить bootstrap облачной синхронизации до V21

--- fa-cloud-sync.js
+++ fa-cloud-sync.js
@@ -1,11 +1,11 @@
-/* FA_CLOUD_SYNC_BOOTSTRAP_V20 */
+/* FA_CLOUD_SYNC_BOOTSTRAP_V21 */
 (function(){
   'use strict';
-  var src='fa-cloud-sync-v18.js?build=20.1';
+  var src='fa-cloud-sync-v18.js?build=21.1';
   var s=document.createElement('script');
   s.src=src;
   s.async=false;
-  s.onload=function(){console.info('[FA] Cloud sync V20 loaded')};
-  s.onerror=function(){console.error('[FA] Cloud sync V20 failed to load',src)};
+  s.onload=function(){console.info('[FA] Cloud sync V21 loaded')};
+  s.onerror=function(){console.error('[FA] Cloud sync V21 failed to load',src)};
   document.head.appendChild(s);
 })();
