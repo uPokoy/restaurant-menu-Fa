740e43d9c5e6c1987f99d973309e933e23b2d9ea
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-06 01:36:08 +0300

    Load realtime sync v25

--- fa-cloud-sync.js
+++ fa-cloud-sync.js
@@ -1,11 +1,11 @@
-/* FA_CLOUD_SYNC_BOOTSTRAP_V24 */
+/* FA_CLOUD_SYNC_BOOTSTRAP_V25 */
 (function(){
   'use strict';
-  var src='fa-cloud-sync-v18.js?build=24.1';
+  var src='fa-cloud-sync-v18.js?build=25.1';
   var s=document.createElement('script');
   s.src=src;
   s.async=false;
-  s.onload=function(){console.info('[FA] Cloud sync V24 loaded')};
-  s.onerror=function(){console.error('[FA] Cloud sync V24 failed to load',src)};
+  s.onload=function(){console.info('[FA] Cloud sync V25 Realtime loaded')};
+  s.onerror=function(){console.error('[FA] Cloud sync V25 Realtime failed to load',src)};
   document.head.appendChild(s);
 })();
