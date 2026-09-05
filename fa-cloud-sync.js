01a7ef42d95e2d48f196751743a3b2d7bef619d6
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-06 01:27:28 +0300

    Обновить bootstrap облачной синхронизации до V22

--- fa-cloud-sync.js
+++ fa-cloud-sync.js
@@ -1,11 +1,11 @@
-/* FA_CLOUD_SYNC_BOOTSTRAP_V21 */
+/* FA_CLOUD_SYNC_BOOTSTRAP_V22 */
 (function(){
   'use strict';
-  var src='fa-cloud-sync-v18.js?build=21.1';
+  var src='fa-cloud-sync-v18.js?build=22.1';
   var s=document.createElement('script');
   s.src=src;
   s.async=false;
-  s.onload=function(){console.info('[FA] Cloud sync V21 loaded')};
-  s.onerror=function(){console.error('[FA] Cloud sync V21 failed to load',src)};
+  s.onload=function(){console.info('[FA] Cloud sync V22 loaded')};
+  s.onerror=function(){console.error('[FA] Cloud sync V22 failed to load',src)};
   document.head.appendChild(s);
 })();
