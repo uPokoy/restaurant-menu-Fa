93f72165033ec643cf1c6be83f027dac6d8c0d20
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-07 16:33:44 +0300

    Add unique V27 loader

---
+++ fa-cloud-sync-v27-loader.js
@@ -0,0 +1,10 @@
+/* FA_CLOUD_SYNC_UNIQUE_LOADER_V27 */
+(function(){
+  'use strict';
+  var s=document.createElement('script');
+  s.src='fa-cloud-sync-v27.js?build=27.2';
+  s.async=false;
+  s.onload=function(){console.info('[FA] Cloud sync V27 unique loader loaded')};
+  s.onerror=function(){console.error('[FA] Cloud sync V27 unique loader failed')};
+  document.head.appendChild(s);
+})();
